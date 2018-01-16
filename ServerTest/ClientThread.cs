0d80831fecbc72371f869cf8d7b46be7cbd8d701
k1601467 <k1601467@C-A4404-14>
2018-01-16 13:45:40 +0200

    hthfg

--- ServerTest/ClientThread.cs
+++ ServerTest/ClientThread.cs
@@ -73,7 +73,7 @@ namespace ServerTest
                         Thread.Sleep(500);
                     
                        
-                    }
+                    
                 }
             }
             
